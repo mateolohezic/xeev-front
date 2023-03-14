b13778214a3c5b87e4fd40f98bd0789110f092e3
Mateo Lohezic <mateolohezic@gmail.com>
2023-03-14 12:08:11 -0300

    problemas

--- xeev-front/src/App.jsx
+++ xeev-front/src/App.jsx
@@ -7,19 +7,21 @@ import Codigos from './Pages/Codigos/Codigos';
 import Admin from './Pages/Admin/Admin';
 import './App.css';
 import Footer from './Components/Footer/Footer';
+import ProblemasTecnicos from './Components/ProblemasTecnicos/ProblemasTecnicos';
 
 function App() {
 
   return (
     <>
-    <NavBar/>
+    <ProblemasTecnicos/>
+    {/* <NavBar/> */}
     <Routes>
       <Route path="/" element={<Inicio/>} />
       <Route path="/Codigo" element={<Codigos/>} />
       <Route path="/Login" element={<Login/>} />
       <Route path="/Admin" element={<Admin/>} />
     </Routes>
-    <Footer/>
+    {/* <Footer/> */}
     </>
   );
 }
---
+++ xeev-front/src/Components/ProblemasTecnicos/ProblemasTecnicos.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './problemasTecnicos.css'
+
+function ProblemasTecnicos() {
+  return (
+    <div className='problemas text-center'>
+        <div className='mt-5'><img src="https://entreobleasyaloloco.files.wordpress.com/2015/11/wpid-wp-1446762894021.jpeg" className='mt-5 imagenMono'/></div>
+        <div className='fs-1 text-muted mt-5 mx-5'><span>Estamos mejorando para usted.</span></div>
+    </div>
+  )
+}
+
+export default ProblemasTecnicos
